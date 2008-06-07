6e60bdd1947798f301c442e3cfd7705bcb14ed40
aapris <aapris@b7be42db-f94e-0410-88db-cdb2fe0d2fab>
2008-06-07 18:13:24 +0000

    svn propset svn:keywords Id make_packages.sh pys60gps.py

--- trunk/make_packages.sh
+++ trunk/make_packages.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #############################################################
+# $Id$
 # Copyright Aapo Rista 2008
 # Create a few different packages which can be used to
 # install pys60gps to a s60-phone.
