ff41ffb2b3a73e596ccab3b88efbb166914466c5
aapris <aapris@b7be42db-f94e-0410-88db-cdb2fe0d2fab>
2009-11-30 08:29:50 +0000

    pys60gpstools.py: simplifies Pys60's position dictionary. locationtools.py: reduce trackpoints.

---
+++ trunk/lib/locationtools.py
@@ -0,0 +1,234 @@
+# -*- coding: utf8 -*-
+"""
+Reduce the number of trackpoints to save.
+
+Full dump of GPS data may take a few megabytes of storage per hour.
+This module contains functions to help to reduce unnecessary
+trackpoints from original tracklog.
+
+After reduction tracklog may contain e.g. only from 1/60 to 5/60
+of original trackpoints, but the track still remains pretty accurate.
+Reduced trackpoints can be restored later by interpolating.
+
+Most functions handle pos dictionaries having keys defined below:
+
+pos = {
+    # Mandatory keys
+    'gpstime': 1259317554.0,
+    'lat': 60.274858047640997,
+    'lon': 24.986528557189999,
+    # Optional
+    'course': 55.709999084472699,
+    'speed': 1.7200000286102299,
+    # Example extra keys
+    'ele': 37.5,
+    'tdop': 2.1199998855590798,
+    'vdop': 2.6800000667571999,
+    'hdop': 1.5900000333786,
+    'sacc': 1.4299999475479099,
+    'hacc': 27.335233688354499,
+    'vacc': 64.5,
+    'cacc': 10.6499996185303,
+    'sat': 7,
+    'satinview': 11,
+}
+
+"""
+
+import sys
+import time
+import math
+
+import pys60gpstools
+
+import Calculate
+import LatLongUTMconversion
+
+# Trigonometric functions
+RAD = math.pi / 180
+
+
+def project_point(x0, y0, dist, angle):
+    """Project a new point from point x0,y0 to given direction and angle."""
+    y1 = y0 + math.cos(angle * RAD) * dist
+    x1 = x0 + math.cos((90 - angle) * RAD) * dist
+    return x1, y1
+
+
+def slope(x0, y0, x1, y1):
+    """Calculate the slope of the line joining two points."""
+    if x0 == x1:
+        return 0
+    return 1.0 * (y0 - y1) / (x0 - x1)
+
+
+def intercept(x, y, a):
+    """Return the y-value (c) where the line intercepts y-axis."""
+    return y - a * x
+
+def distance(a, b, c, m, n):
+    """"""
+    return abs(a * m + b * n + c) / math.sqrt(a ** 2 + b ** 2)
+
+
+def distance_from_vector(x0, y0, dist, angle, x, y):
+    """
+    Project a new point using x0,y0,dist,angle and return the distance how far
+    point x,y is from line between x0,y0 and the projected point.
+    """
+    x1, y1 = project_point(x0, y0, dist, angle)
+    a = slope(x0, y0, x1, y1)
+    c = intercept(x0, y0, a)
+    dist = distance(a, -1, c, x, y)
+    return dist
+
+
+def distance_from_line(x0, y0, x1, y1, x, y):
+    """Return the distance from line x0,y0,x1,y1 to point x,y."""
+    a = slope(x0, y0, x1, y1)
+    c = intercept(x0, y0, a)
+    return distance(a, -1, c, x, y)
+
+
+def set_fake_utm(pos, long_origin=None):
+    """
+    Save "fake" UTM coordinates to pos. Those are used in
+    triginometic calculations later.
+    """
+    if long_origin is None:
+        long_origin = pos['lon']
+    (pos["z"],
+     pos["e"],
+     pos["n"]) = LatLongUTMconversion.LLtoUTM(23, # Wgs84
+                                              pos['lat'], pos['lon'],
+                                              long_origin)
+
+
+# Helpers
+def pos_distance(pos1, pos2):
+    """Return distance between two pos objects, calculated from lat/lon."""
+    return Calculate.distance(pos1['lat'], pos1['lon'],
+                              pos2['lat'], pos2['lon'])
+
+
+def pos_distance_from_line(p1, p2, pos):
+    """
+    Return pos' distance from line (p1,p2), calculated from
+    fake UTM coordinates.
+    """
+    return distance_from_line(p1["e"], p1["n"],
+                              p2["e"], p2["n"],
+                              pos["e"], pos["n"])
+
+
+# Handler
+def handle_trkpt(pos, tracklog, limits, long_origin):
+    """
+    Compare pos to 1-2 latest points in tracklog and append pos to it
+    if certain conditions are met.
+    """
+    set_fake_utm(pos, long_origin)
+    # New trackpoint if tracklog is empty or has only 1 point yet
+    if len(tracklog) <= 1:
+        pos['reason'] = u"Startpoint"
+        tracklog.append(pos)
+        return 1
+    # New trackpoint max_time has been exceeded
+    timediff = pos['gpstime'] - tracklog[-1]['gpstime']
+    if timediff >= limits['max_time']:
+        pos['reason'] = u"Timediff %.2f" % (timediff)
+        tracklog.append(pos)
+        return 2
+    # New trackpoint if dist between this and latest saved exceeds threshold
+    last_dist = pos_distance(pos, tracklog[-1])
+    if last_dist >= limits['max_dist']:
+        pos['reason'] = u"Distance %2.2f>%.2f" % (last_dist,
+                                                  limits['max_dist'])
+        tracklog.append(pos)
+        return 3
+    # New trackpoint if max_trackdiff far from line between 2 latest points
+    dist_line = pos_distance_from_line(tracklog[-1], tracklog[-2], pos)
+    if dist_line >= limits['max_trackdiff']:
+        pos['reason'] = u"Distline %2.2f>%.2f" % (dist_line,
+                                                  limits['max_trackdiff'])
+        tracklog.append(pos)
+        return 4
+    # New trackpoint if turning
+    anglediff = Calculate.anglediff(tracklog[-1]['course'], pos["course"])
+    if anglediff > limits['max_anglediff'] and last_dist > limits['min_dist']:
+        pos['reason'] = u"Anglediff %2.2f>%.2f" % (anglediff,
+                                                   limits['max_anglediff'])
+        tracklog.append(pos)
+        return 5
+    # TODO:
+    # - speed diff
+    # - dynamic limits?
+
+
+def _get_poslist():
+    """
+    Read the raw track data (1 trackpoint per line in json-format) from file.
+    """
+    import simplejson
+    simple_list = []
+    with open(sys.argv[1], "rt") as trackjson:
+        for line in trackjson:
+            pos = simplejson.loads(line)
+            simple_list.append(pys60gpstools.simplify_position(pos))
+    return simple_list
+
+if __name__ == '__main__':
+    POSLOG = []
+    TRACKLOG = []
+    LOSTFIX = True
+    LONG_ORIGIN = None
+
+    # Track simplification parameters
+    LIMITS = {
+        'max_trackdiff': 10.0, # meters
+        'min_dist': 10.0, # meters
+        'max_dist': 1000.0, # meters
+        'min_time': 0.0, # seconds
+        'max_time': 60.0, # seconds
+        'max_anglediff': 30.0, # degrees
+    }
+    for POS in _get_poslist():
+        if pys60gpstools.has_fix(POS):
+            if LOSTFIX == True:
+                POS["segstart"] = True
+            POSLOG.append(POS)
+            LOSTFIX = False
+        else:
+            LOSTFIX = True
+        #print LOSTFIX
+    if POSLOG:
+        LONG_ORIGIN = POSLOG[0]['lon']
+    for POS in POSLOG:
+        if handle_trkpt(POS, TRACKLOG, LIMITS, LONG_ORIGIN):
+            pass
+            #print POS['reason']
+    POS['reason'] = "Last"
+    TRACKLOG.append(POS) # add last one
+    # print "<!--Tracklog len %d-->" % len(TRACKLOG)
+    import track2kml
+    print track2kml.header()
+    print track2kml.start_placemark("All")
+    for POS in _get_poslist():
+        if pys60gpstools.has_fix(POS):
+            print "%(lon).6f,%(lat).6f" % POS
+    PLACEMARKS = []
+    print track2kml.end_placemark()
+    print track2kml.start_placemark("Reduced", 'ffffffff')
+    for POS in TRACKLOG:
+        print "%(lon).6f,%(lat).6f" % POS
+        #print POS
+        des = time.strftime(u"%H:%M:%S ", time.localtime(POS['gpstime']))
+        des += u"%(speed).1f m/s %(course).1f° %(hdop).1f" % POS
+        PLACEMARKS.append(track2kml.placemark({
+            'name': "%s" % POS['reason'][:1],
+            'description': des,
+            'coordinates': "%(lon).6f,%(lat).6f" % POS,
+        }))
+    print track2kml.end_placemark()
+    print "\n".join(PLACEMARKS).encode("utf8")
+    print track2kml.footer()
---
+++ trunk/lib/pys60gpstools.py
@@ -0,0 +1,107 @@
+# -*- coding: utf8 -*-
+"""
+Miscancellous functions to manipulate PyS60's positioning dicts.
+
+Positioning.position() function returns dicts like this:
+{'position': {'vertical_accuracy': 64.5, 'horizontal_accuracy': 27.335233688354499, 'altitude': 37.5, 'longitude': 24.986528557189999, 'latitude': 60.274858047640997}, 'course': {'speed': 1.7200000286102299, 'heading': 55.709999084472699, 'heading_accuracy': 10.6499996185303, 'speed_accuracy': 1.4299999475479099}, 'satellites': {'horizontal_dop': 1.5900000333786, 'used_satellites': 7, 'vertical_dop': 2.6800000667571999, 'time': 1259317554.0, 'satellites': 11, 'time_dop': 2.1199998855590798}}
+
+However, this is a bit too verbose (long variable names), so
+simplify_position() function converts above dict to one like below:
+
+{'gpstime': 1259317554.0, 'tdop': 2.1199998855590798, 'vdop': 2.6800000667571999, 'hdop': 1.5900000333786, 'sacc': 1.4299999475479099,
+ 'lat': 60.274858047640997, 'ele': 37.5, 'course': 55.709999084472699, 'satinview': 11, 'cacc': 10.6499996185303, 'lon': 24.986528557189999, 'vacc': 64.5, 'sat': 7, 'speed': 1.7200000286102299, 'hacc': 27.335233688354499}
+
+Now you can use pos['lat'] instead of pos['position']['latitude'].
+"""
+
+import sys
+import time
+
+# Dictionary key mappings
+pos_gpx_map = {}
+pos_gpx_map['satellites'] = {
+    'horizontal_dop' : 'hdop',
+    'used_satellites' : 'sat',
+    'vertical_dop' : 'vdop',
+    'time_dop' : 'tdop',
+    'satellites' : 'satinview',
+    'time' : 'gpstime',
+}
+
+pos_gpx_map['position'] = {
+    'altitude' : 'ele',
+    'latitude' : 'lat',
+    'longitude' : 'lon',
+    'vertical_accuracy' : 'vacc',
+    'horizontal_accuracy' : 'hacc',
+}
+
+pos_gpx_map['course'] = {
+    'speed' : 'speed',
+    'heading' : 'course',
+    'heading_accuracy' : 'cacc', # course accuracy
+    'speed_accuracy' : 'sacc',
+}
+
+def isnan(n):
+    """
+    Return True if n is float and one of NaN, Inf or -Inf.
+    Note: this is probably non-portable kludge, which is ment to work
+    when sys.platform == 'symbian_s60'
+
+    >>> inf = 1e300000
+    >>> nan = inf/inf
+    >>> isnan(nan)
+    True
+    >>> isnan(inf)
+    True
+    >>> isnan(-inf)
+    True
+    >>> isnan(1.2345)
+    False
+    >>> isnan("foo bar")
+    False
+    >>> isnan(42)
+    False    
+    """
+    if isinstance(n, float) and str(n).lower() in ['nan', 'inf', '-inf']:
+        return True
+    else:
+        return False
+
+def isfloat(n):
+    """Return True if n is a real float."""
+    if isinstance(n, float) and str(n).lower() not in ['nan', 'inf', '-inf']:
+        return True
+    else:
+        return False
+    
+def has_fix(pos):
+    """
+    Return True if pos has lat and lon keys and both are real floats
+    (not NaN or Inf).
+    """
+    if 'lat' in pos and isfloat(pos['lat']) and \
+       'lon' in pos and isfloat(pos['lon']) :
+        return True
+    else:
+        return False
+
+def simplify_position(pos, include_nans=False):
+    """Convert PyS60's position dict to simplier dict and return it."""
+    new_pos = {}
+    for section in pos.keys(): # [satellites, position, course]
+        if section in pos_gpx_map:
+            # Loop all keys in pos_gpx_map and convert them to fit flat style
+            for key in pos_gpx_map[section].keys():
+                if key in pos[section] and \
+                       (not isnan(pos[section][key]) or \
+                        include_nans == True):
+                    new_pos[pos_gpx_map[section][key]] = pos[section][key]
+    return new_pos
+
+def _test():
+    print "FIXME: Not implemented yet"
+
+if __name__=='__main__':
+    _test()
